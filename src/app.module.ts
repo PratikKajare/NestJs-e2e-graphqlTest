3b8cc7d491d65d1e475973d6bd52c9ad72ea2dc5
PratikKajare <pratikkajare826@gmail.com>
2022-08-16 12:40:46 +0530

    Add application and DB configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
+import { ConfigurationModule } from './config/config.module';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [BooksModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `${process.cwd()}/.env`,
+      isGlobal: true,
+      load: [configuration],
+    }),
+    ConfigurationModule,
+    BooksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ApplicationConfig } from './config/interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const appPort = configService.get<ApplicationConfig>('application').port;
+  console.log(appPort);
+
+  await app.listen(appPort);
 }
 bootstrap();
